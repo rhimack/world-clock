65f33f94eb70d673266188aafc162781557244f7
rhimack <remackness@outlook.com>
2025-01-19 17:03:42 +0000

    Added additional city & improved JS when select is selected so that no 'undefined' appears

--- javascript/index.js
+++ javascript/index.js
@@ -26,18 +26,36 @@ function updateTime() {
   }
 }
 
+let parisElement = document.querySelector("#paris");
+if (parisElement) {
+  let parisElementDateElement = parisElement.querySelector(".date");
+  let parisElementTimeElement = parisElement.querySelector(".time");
+  let parisTime = moment().tz("Europe/Paris");
+
+  parisElementDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
+
+  parisElementTimeElement.innerHTML = parisTime.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
     cityTimeZone = moment.tz.guess();
   }
 
+  if (cityTimeZone === "select") {
+    cityTimeZone.remove();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `<div class="city">
           <div>
-            <h2>${cityTimeZone}</h2>
+            <h2>${cityName}</h2>
             <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
           </div>
           <div class="time">${cityTime.format(
